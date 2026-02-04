cdead0ad8bfe714355bfee58b6ae59de1167fb7f
Nigam Chovatiya <nigamchovatiya@gmail.com>
2026-02-04 12:18:06 +0530

    create table using models 04-02-26

--- learning26/learning26/settings.py
+++ learning26/learning26/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'student', # add this line for student app
 ]
 
 MIDDLEWARE = [
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Student(models.Model):
+    studentName = models.CharField(max_length=50)
+    studentAge = models.IntegerField()
+    studentCity = models.CharField(max_length=40)
+    studentEmail = models.EmailField(null=True)
+
+    #meta class
+    class Meta:
+        db_table = "student" #table name in database
+
+
+class Product(models.Model):
+    productName = models.CharField(max_length=50)
+    productPrice = models.IntegerField()
+    productDescription = models.TextField()
+    productStock = models.PositiveIntegerField()
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+
+    #meta class
+    class Meta:
+        db_table = "product" #table name in database    
+
+
+class Car(models.Model):
+    carName = models.CharField(max_length=50)
+    carPrice = models.PositiveIntegerField()
+    carCompany = models.CharField(max_length=40)
+
+    #meta class
+    class Meta:
+        db_table = "car" #table name in database           
