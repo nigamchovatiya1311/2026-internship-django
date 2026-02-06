9447179b3b09fe2c0bda62cd646aa04ae93cd726
Nigam Chovatiya <nigamchovatiya@gmail.com>
2026-02-06 19:52:43 +0530

    create a one-to-one and one-to-many relation table in app.

--- learning26/student/admin.py
+++ learning26/student/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import Student,Product,Car
+from .models import Student,Product,Car,StudentProfile,Category,Service,Booking,Payment,Offer,BookingOrder,Customer,Purchase
 
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Product)
-admin.site.register(Car)    
+admin.site.register(Car) 
+admin.site.register(StudentProfile) 
+admin.site.register(Category)
+admin.site.register(Service) 
+admin.site.register(Booking)
+admin.site.register(Payment) 
+admin.site.register(Offer)
+admin.site.register(BookingOrder)
+admin.site.register(Customer)
+admin.site.register(Purchase)
--- learning26/student/models.py
+++ learning26/student/models.py
@@ -13,6 +13,9 @@ class Student(models.Model):
     class Meta:
         db_table = "student" #table name in database
 
+    def __str__(self):
+        return self.studentName          
+
 
 class Product(models.Model):
     productName = models.CharField(max_length=50)
@@ -34,4 +37,162 @@ class Car(models.Model):
 
     #meta class
     class Meta:
-        db_table = "car" #table name in database           
+        db_table = "car" #table name in database     
+
+
+# One to One relationship
+
+hobbies = (('cricket','Cricket'),('football','Football'),('badminton','Badminton'))
+class StudentProfile(models.Model):
+    # one to one relationship with student table
+    studentId = models.OneToOneField(Student,on_delete=models.CASCADE)
+    studentHobbies = models.CharField(max_length=20,choices=hobbies)
+    studentAddress = models.TextField(max_length=100)
+    studentPhone = models.CharField(max_length=10)
+    studentGender = models.CharField(max_length=10)
+    studentDOB = models.DateField()
+
+    #meta class
+    class Meta:
+        db_table = "studentprofile" #table name in database
+
+    def __str__(self):
+        return self.studentId.studentName
+    
+
+# one to one relationship ex.
+
+class Booking(models.Model):
+    bookingId = models.AutoField(primary_key=True)
+    bookingDate = models.DateField()
+    bookingTime = models.TimeField()
+    # one to one relationship with studentprofile table
+    
+    #meta class
+    class Meta:
+        db_table = "booking"
+
+    def __str__(self):
+        return str(self.bookingId)
+    
+class Payment(models.Model):
+    paymentId = models.AutoField(primary_key=True)
+    paymentAmount = models.IntegerField()
+    paymentDate = models.DateField()
+    paymentTime = models.TimeField()
+    # one to one relationship with booking table
+    bookingId = models.OneToOneField(Booking,on_delete=models.CASCADE)
+
+    #meta class
+    class Meta:
+        db_table = "payment"
+
+    def __str__(self):
+        return str(self.paymentId)    
+        
+
+
+
+# One to Many relationship
+class Category(models.Model):
+    categoryName = models.CharField(max_length=100)
+    categoryDescription = models.TextField()
+    categoryStatus = models.BooleanField(default=True)
+    
+    #meta class
+    class Meta:
+        db_table = "category"
+
+    def __str__(self):
+        return self.categoryName   
+
+
+class Service(models.Model):
+    serviceName = models.CharField(max_length=100)
+    serviceDescription = models.TextField()
+    servicePrice = models.IntegerField()
+    serviceStatus = models.BooleanField(default=True)
+    discount = models.IntegerField(null=True)
+    #foreign key to connect one to many relationship category table
+    categoryId = models.ForeignKey(Category,on_delete=models.CASCADE)
+
+    #meta class
+    class Meta:
+        db_table = "service"
+
+    def __str__(self):
+        return self.serviceName   
+    
+
+# one to many relationship ex.
+
+class Offer(models.Model):
+    offerid = models.AutoField(primary_key=True)
+    offerName = models.CharField(max_length=100)
+    offerDescription = models.TextField()
+    offerValidfrom = models.DateField()
+    offerValidto = models.DateField()
+
+    #meta class
+    class Meta:
+        db_table = "offer"
+
+    def __str__(self):
+        return str(self.offerid)
+    
+class BookingOrder(models.Model):
+    orderId = models.AutoField(primary_key=True)
+    orderDate = models.DateField()
+    # foreign key to connect one to many relationship with offer table
+    offerId = models.ForeignKey(Offer,on_delete=models.CASCADE)
+
+    #meta class
+    class Meta:
+        db_table = "bookingorder"
+
+    def __str__(self):
+        return str(self.orderId)
+    
+ #ex--2 
+
+class Customer(models.Model):
+    customerId = models.AutoField(primary_key=True)
+    customerName = models.CharField(max_length=100)
+    customerEmail = models.EmailField()
+    customerPhone = models.CharField(max_length=10)
+
+    #meta class
+    class Meta:
+        db_table = "customer"
+
+    def __str__(self):
+        return self.customerName  
+    
+class Purchase(models.Model):
+    purchaseId = models.AutoField(primary_key=True)
+    purchaseDate = models.DateField()
+    purchaseAmount = models.FloatField()
+    # foreign key to connect one to many relationship with customer table
+    customerId = models.ForeignKey(Customer,on_delete=models.CASCADE)
+
+    #meta class
+    class Meta:
+        db_table = "purchase"
+
+    def __str__(self):
+        return str(self.purchaseId)    
+
+
+
+    
+
+   
+
+
+
+
+
+
+
+    
+    
